24634d20b4a83a8640baddd10c39df976146228b
Steve C <diceroll123@gmail.com>
2024-02-19 00:04:27 -0500

    Update food_adjustments.rs

--- src/food_adjustments.rs
+++ src/food_adjustments.rs
@@ -2,7 +2,7 @@
 // the first dimension is the pirate ID,
 // the second dimension is the food ID
 
-pub static POSITIVE_FOOD: [[u8; 40]; 20] = [
+pub const POSITIVE_FOOD: [[u8; 40]; 20] = [
     [
         2, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
         0, 0, 2, 0, 0, 0, 0, 0, 0, 0,
@@ -85,7 +85,7 @@ pub static POSITIVE_FOOD: [[u8; 40]; 20] = [
     ],
 ];
 
-pub static NEGATIVE_FOOD: [[u8; 40]; 20] = [
+pub const NEGATIVE_FOOD: [[u8; 40]; 20] = [
     [
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
         0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
