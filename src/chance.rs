ed49108f7b5dbed4235719280f09cad4dd01e18f
Steve C <diceroll123@gmail.com>
2024-02-24 16:53:41 -0500

    Make Chance values fully public

--- src/chance.rs
+++ src/chance.rs
@@ -1,8 +1,8 @@
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
 pub struct Chance {
-    pub(crate) value: u32,
-    pub(crate) probability: f64,
-    pub(crate) cumulative: f64,
-    pub(crate) tail: f64,
+    pub value: u32,
+    pub probability: f64,
+    pub cumulative: f64,
+    pub tail: f64,
 }
