07db430e6ee19cf6ab1af1bf086b78e1ed0dc7b3
Steve C <diceroll123@gmail.com>
2024-03-23 13:59:04 -0400

    Expose expand_ib_object function as public

--- src/math.rs
+++ src/math.rs
@@ -335,7 +335,7 @@ fn ib_prob(binary: u32, probabilities: [[f64; 5]; 5]) -> f64 {
     total_prob
 }
 
-fn expand_ib_object(bets: &[[u8; 5]], bet_odds: &[u32]) -> HashMap<u32, u32> {
+pub fn expand_ib_object(bets: &[[u8; 5]], bet_odds: &[u32]) -> HashMap<u32, u32> {
     // makes a dict of permutations of the pirates + odds
     // this is why the bet table could be very long
 
